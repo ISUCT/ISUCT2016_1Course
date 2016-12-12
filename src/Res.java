549055ae39d527da572276c2c4b0e0c562b4b251
BPavelS <asdasddddqwerty@gmail.com>
2016-12-12 11:31:12 +0400

    src/Calculator.java

---
+++ src/Res.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/**
+ * Created by stud_6 on 12.12.16.
+ */
+public class Res {
+    public static void main(String[] strings){
+        Calculator calcul = new Calculator();
+        double y;
+        y = calcul.calc(1,2);
+        System.out.println(y);
+    }
+}
