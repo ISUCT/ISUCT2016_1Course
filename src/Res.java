1e1d0c2c15c4c6d892d3ff8a31b0b0ddccfd6d19
Bulashev Pavel <asdasddddqwerty@gmail.com>
2016-12-18 22:45:05 +0400

    Calculator.java/Res.java
    Res-main class
    Calculator-claac, method calc(перегруз с передачей либо арэйлиста, либо единственного значения)

--- src/Res.java
+++ src/Res.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
  * Created by stud_6 on 12.12.16.
  */
 public class Res {
+
     public static void main(String[] strings){
+        ArrayList<Double> xZn = new ArrayList<Double>();
+        xZn.add(1.1);
+        xZn.add(2.4);
+        xZn.add(3.6);
+        xZn.add(1.7);
+        xZn.add(3.9);
         Calculator calcul = new Calculator();
-        double y;
-        y = calcul.calc(1,2);
-        System.out.println(y);
+        calcul.calc(xZn);
     }
 }
