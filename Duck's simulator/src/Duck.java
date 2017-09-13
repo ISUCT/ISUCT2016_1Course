10eac89a1a9ad64feedaa07cadd1e10329742618
Your Name <you@example.com>
2017-09-13 11:30:53 +0300

    edite DuckSim

---
+++ Duck's simulator/src/Duck.java
@@ -0,0 +1,26 @@
+/**
+ * Created by UI on 13.09.2017.
+ * @noinspection ALL
+ */
+public abstract class Duck {
+    FlyBehaviour flyBehaviour;
+    QuackBehaviour quackBehaviour;
+    public Duck{
+    }
+        public abstract void display();
+        public class perfomFly(){
+        flyBehaviour.fly();
+    }
+    public void performQuack(){
+        quackBehaviour.quack();
+    }
+    public void Swim(){
+        System.out.println("All ducks float, even decoys!");
+    }
+    public void setFlyBehaviour(FlyBehaviour fb){
+        flyBehaviour = fb;
+    }
+    public void setQuackBehaviour(QuackBehaviour qb){
+        quackBehaviour = qb;
+    }
+}
