a303b57d4383b12215655c4a1e94b2d8a13d7991
Ipatov M <maxmm81@gmail.com>
2017-09-27 06:56:22 +0300

    Test

--- src/main/java/OOP/Duck.java
+++ src/main/java/OOP/Duck.java
@@ -8,6 +8,8 @@ public abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
 
+    public Duck(){}
+
     public void setFlyBehavior(FlyBehavior fb){
         flyBehavior = fb;
     }
