31ab23da18d317eb6e38f5003f2a8fae9810d798
anaitya <an-it-ka@mail.ru>
2017-11-06 11:07:35 +0300

    Add files via upload

---
+++ src/CurrentConditionsDisplay.java
@@ -0,0 +1,9 @@
+package Ww;
+
+/**
+ * Created by UI on 06.11.2017.
+ */
+public class CurrentConditionsDisplay {
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+    }
+}
---
+++ src/StatisticsDisplay.java
@@ -0,0 +1,9 @@
+package Ww;
+
+/**
+ * Created by UI on 06.11.2017.
+ */
+public class StatisticsDisplay {
+    public StatisticsDisplay(WeatherData weatherData) {
+    }
+}
